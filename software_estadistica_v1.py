eb073920a0b3c99c22da4a0c877957298086fa88
Agustín Bernasconi <bernasconiagustin0@gmail.com>
2024-05-25 00:46:11 -0300

    Commit 2: funciones de media, moda, cuartiles y frecuencias. Cambio en el menú.

--- software_estadistica_v1.py
+++ software_estadistica_v1.py
@@ -1,8 +1,31 @@
-conteo = {}
-conteo_ordenado = {}
-nros = []
+# Definición de funciones
 
-def mediana(lista: set) -> float:
+def frecuencias(lista_ordenada: dict) -> set:
+    can = sum(lista_ordenada.values())
+    frecuencia_abs_acumulada = 0
+    frecuencia_rel_acumulada = 0
+    frecuencia_por_acumulada = 0
+    filas = []
+
+    for numero, frecuencia in lista_ordenada.items():
+        frecuencia_relativa = (frecuencia / can)
+        frecuencia_porcentual = (frecuencia / can) * 100
+        frecuencia_abs_acumulada += frecuencia
+        frecuencia_rel_acumulada += frecuencia_relativa
+        frecuencia_por_acumulada += frecuencia_porcentual
+        fila = f"{numero:6} | {frecuencia:2}  | {frecuencia_relativa:7.4f} | {frecuencia_porcentual:7.4f}% | {frecuencia_abs_acumulada:3}  | {frecuencia_rel_acumulada:7.4f}  | {frecuencia_por_acumulada:7.4f}%"
+        filas.append(fila)
+
+    return filas
+
+def media(lista: list) -> float:
+    suma = sum(lista)
+    res_media = suma / len(lista)
+    res_media_redondeado = round(res_media, 4)
+
+    return res_media_redondeado
+
+def mediana(lista: list) -> float:
     lista.sort()
     v_central = len(lista) // 2
 
@@ -13,6 +36,37 @@ def mediana(lista: set) -> float:
 
     return res
     
+def moda(lista: dict, lista_ordenada: dict) -> list:
+    max_frecuencia = max(lista.values())
+    res_moda = [numero for numero, frecuencia in lista_ordenada.items() if frecuencia == max_frecuencia]
+
+    return res_moda, max_frecuencia
+
+def cuartiles(lista: list) -> float:
+    lista.sort()
+    mitad = len(lista) // 2
+
+    if len(lista) % 2 == 0:
+        nros_izq = lista[:mitad]
+    else:
+        nros_izq = lista[:mitad+1]
+    Q1 = mediana(nros_izq)
+
+    Q2 = mediana(lista)
+
+    nros_der = lista[mitad:]
+    Q3 = mediana(nros_der)
+
+    return Q1, Q2, Q3
+
+# Definición de variables globales
+
+conteo = {}
+conteo_ordenado = {}
+nros = []
+
+# Lógica principal
+
 can = int(input("Ingrese la cantidad de datos que quiere usar: "))
 for i in range(can):
         nro = float(input("Ingrese el dato: "))
@@ -30,92 +84,106 @@ for clave in claves_ordenadas:
      conteo_ordenado[clave] = conteo[clave]
 
 while True:
-    opcion = int(input("""\nSeleccione qué desea realizar:
+    opcion_a = int(input("""\nSeleccione qué tipo de medida quiere calcular:
+                       
     0 = Salir
-    1 = Frecuencias                   
-    2 = Media
-    3 = Mediana
-    4 = Moda
-    5 = Cuartiles                   
+    1 = Medidas de posición         
+    2 = Medidas de dispersión  
+                                        
 Opción elegida: """))
 
-    if opcion == 0:
+    if opcion_a == 0:
         print("Cerrando aplicación")
         break
 
-    elif opcion == 1:
-        frecuencia_abs_acumulada = 0
-        frecuencia_rel_acumulada = 0
-        frecuencia_por_acumulada = 0
+    elif opcion_a == 1:
 
-        print("\nFrecuencias:")
-        print(" Número | fi  |   fri   |  fri%    |  Fi  |   Fri    |   Fri%  ")
-        print("--------------------------------------------------------------")
+        opcion_b = int(input("""\nSeleccione qué medida de posición quiere calcular:
+                       
+    0 = Salir
+    1 = Media         
+    2 = Mediana
+    3 = Moda                           
+                                        
+Opción elegida: """))
+        
+        if opcion_b == 1:
 
-        for numero, frecuencia in conteo_ordenado.items():
-                  frecuencia_relativa = (frecuencia / can) 
-                  frecuencia_porcentual = (frecuencia / can) * 100 
-                  frecuencia_abs_acumulada += frecuencia 
-                  frecuencia_rel_acumulada += frecuencia_relativa 
-                  frecuencia_por_acumulada += frecuencia_porcentual 
-                  print(f"{numero:6}  | {frecuencia:2}  | {frecuencia_relativa:7.4f} | {frecuencia_porcentual:7.4f}% | {frecuencia_abs_acumulada:3}  | {frecuencia_rel_acumulada:7.4f}  | {frecuencia_por_acumulada:7.4f}%")
+            res_media = media(nros) 
+            print("\nLa media de estos datos es de " + str(res_media))
+            input("Presione enter para continuar ")
 
-        input("Presione enter para continuar ")      
-            
-    elif opcion == 2:
+        elif opcion_b == 2:
 
-        suma = sum(nros)
-        media = suma / can
-        print("\nLa media de estos datos es de " + str(media))
-        input("Presione enter para continuar ")
+            res_mediana = mediana(nros)
+            print("\nLa mediana es " + str(res_mediana))
+            input("Presione enter para continuar ")
 
-    elif opcion == 3:  
-        res_mediana = mediana(nros)
+        elif opcion_b == 3:
 
-        print("\nLa mediana es " + str(res_mediana))
-        input("Presione enter para continuar ")
+            res_moda, max_frecuencia = moda(conteo, conteo_ordenado)
+            print("")
+            for valor in res_moda:
+                print("Moda:", valor)
 
-    elif opcion == 4:
-        moda = []
-        # Se calcula cual es el valor más alto
-        max_frecuencia = max(conteo.values())
+            print("\nFrecuencia:", max_frecuencia)
+            input("Presione enter para continuar ")
 
-        # Se hace un for en el diccionario buscando cuales son los números con mayor frecuencia.
-
-        # Forma N*1
-        moda = [numero for numero, frecuencia in conteo_ordenado.items() if frecuencia == max_frecuencia]
-
-        print("\nLa moda es", moda, "\nLa frecuencia es:", max_frecuencia)
-        input("Presione enter para continuar ")
-
-    elif opcion == 5:
-        nros.sort()
-        mitad = can // 2
-
-        # Calcular Q1
-        if can % 2 == 0:
-            nros_izq = nros[:mitad]
         else:
-            nros_izq = nros[:mitad+1]
-    
-        Q1 = mediana(nros_izq)   
+            print("Opción no válida, por favor seleccione una opción válida.")
 
-        # Calcular Q2 (Mediana)
-        Q2 = mediana(nros)
+    elif opcion_a == 2:
 
-        # Calcular Q3
-        nros_der = nros[mitad:]
+        opcion_b = int(input("""\nSeleccione qué medida de dispersión quiere calcular:
+                       
+    0 = Salir
+    1 = Rango         
+    2 = Varianza
+    3 = Desviación estándar
+    4 = Cuartiles
+    5 = Tabla de frecuencias                       
+                                        
+Opción elegida: """))
+        
+        if opcion_b == 1:
+            pass # hacer rango
 
-        Q3 = mediana(nros_der)
+        elif opcion_b == 2:
+            pass # hacer varianza
 
-        print(f"\nQ1 (25%): {Q1}")
-        print(f"Q2 (50%, mediana): {Q2}")
-        print(f"Q3 (75%): {Q3}")
-        input("Presione enter para continuar ")
+        elif opcion_b == 3:
+            pass # hacer desviación
 
-    elif opcion == 6: # Desvio estandar, varianza, rango...
-        pass
-    
+        elif opcion_b == 4:
+
+            res_Q1, res_Q2, res_Q3 = cuartiles(nros)
+            print(f"\nQ1 (25%): {res_Q1}")
+            print(f"Q2 (50%, mediana): {res_Q2}")
+            print(f"Q3 (75%): {res_Q3}")
+            input("Presione enter para continuar ")
+
+        elif opcion_b == 5:
+
+            tabla = "\nFrecuencias:" +\
+                "\nNúmero | fi  |   fri   |  fri%    |  Fi  |   Fri    |   Fri%" +\
+                "\n---------------------------------------------------------------"
+            print(tabla)
+
+            filas = frecuencias(conteo_ordenado)
+            for fila in filas:
+                print(fila)
+
+            input("\nPresione enter para continuar ")
+        
+        else:
+            print("Opción no válida, por favor seleccione una opción válida.")
+
     else:
         print("Opción no válida, por favor seleccione una opción válida.")
+
+
+
+
+
         
+    
